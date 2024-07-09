2e5c9e28f47f138067cafb9ff2b3f3527eef02f1
Murikigy <mgy20@inf.ufpr.br>
2024-07-09 18:58:14 -0300

    Juntado codigo com a biblioteca thrust

--- reduceMax/compila.sh
+++
@@ -1,3 +0,0 @@
-echo nvcc -arch sm_61 --std=c++14 -o cudaReduceMax cudaReduceMax.cu 
-nvcc -arch sm_61 --std=c++14 -o cudaReduceMax cudaReduceMax.cu 
-
--- reduceMax/cudaReduceMax.cu
+++ reduceMax/cudaReduceMax.cu
@@ -1,3 +1,8 @@
+#include <thrust/device_ptr.h>
+#include <thrust/extrema.h>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+
 #include <cstdio>
 #include <cstdlib>
 #include <stdio.h>
@@ -38,16 +43,17 @@ __global__ void reduceMax_atomic_persist(float *max, float *input, int nElements
 int main(int argc, char **argv) {
   cudaError_t err = cudaSuccess;            // Check return values for CUDA calls
   float *h_input = NULL, *d_input = NULL;   // Host and device vectors
-  float h_max, *d_max;                     // Host and device max
+  float h_max, *d_max;                      // Host and device max
   float max = 0;                            // max value
   u_int numElements = atoi(argv[1]);
   u_int nR = atoi(argv[2]);
+  chronometer_t chrono;                     // Chronometer
 
   printf("Running reduceMax for %d elements\n", numElements);
   size_t size = numElements * sizeof(float);
 
   // INICIA VARIAVEIS LOCAIS
-
+  
   // Allocate the host input vector A and check
   h_input = (float *)malloc(size);
   if ( CHECK(h_input == NULL, "Failed to allocate host vectors!\n") )
@@ -55,7 +61,7 @@ int main(int argc, char **argv) {
 
   // Initialize the host input vectors
   for (int i = 0; i < numElements; ++i) {
-    h_input[i] = rand();
+    h_input[i] = rand() % 100;
     if ( h_input[i] > max ) { max = h_input[i]; }
   }
 
@@ -77,17 +83,21 @@ int main(int argc, char **argv) {
   if ( CHECK(err != cudaSuccess, "Failed to copy vector A from host to device (error code %s)!\n", cudaGetErrorString(err)) )
     exit(EXIT_FAILURE);
 
+  // Initialize thrust variables
+  thrust::device_ptr<float> thrust_d_ptr(d_input);
+  thrust::device_vector<float> thrust_d_input(thrust_d_ptr, thrust_d_ptr+numElements);
+
   printf("Launching CUDA kernels with %d blocks of %d threads\n", NP*BLOCKS, THREADS);
 
   // EXECUTE PERSIST ============================
 
   printf("\n === EXECUTANDO KERNEL PERSIST ===\n");
-  chronometer_t chrono_reduce;
-  chrono_start(&chrono_reduce);
+  chrono_reset(&chrono);
+  chrono_start(&chrono);
   for (int i = 0; i < nR; ++i) 
     reduceMax_persist<<<NP*THREADS, THREADS>>>(d_max, d_input, numElements);
   cudaDeviceSynchronize();
-  chrono_stop(&chrono_reduce);
+  chrono_stop(&chrono);
   err = cudaGetLastError();
   if ( CHECK(err != cudaSuccess, "Failed to launch reduceMax_persist kernel (error code %s)!\n", cudaGetErrorString(err)) )
     exit(EXIT_FAILURE);
@@ -104,20 +114,20 @@ int main(int argc, char **argv) {
   } else { printf("Max value: %.6f\n", h_max); }
 
   printf("Tempo médio por ativação do kernel" );
-  chrono_report_TimeInLoop( &chrono_reduce, (char *)"reduceMax_persist", nR);
-  double reduce_time_seconds = (double) chrono_gettotal( &chrono_reduce )/((double)1000*1000*1000);
+  chrono_report_TimeInLoop( &chrono, (char *)"reduceMax_persist", nR);
+  double reduce_time_seconds = (double) chrono_gettotal( &chrono )/((double)1000*1000*1000);
   printf( "Total_time_in_seconds: %lf s\n", reduce_time_seconds );
   printf( "Throughput: %lf INT/s\n", (numElements)/reduce_time_seconds );
 
   // EXECUTE ATOMIC =============================
 
   printf("\n === EXECUTANDO KERNEL ATOMIC ===\n");
-  chronometer_t chrono_atomic;
-  chrono_start(&chrono_atomic);
+  chrono_reset(&chrono);
+  chrono_start(&chrono);
   for (int i = 0; i < nR; ++i) 
     reduceMax_persist<<<NP*THREADS, THREADS>>>(d_max, d_input, numElements);
   cudaDeviceSynchronize();
-  chrono_stop(&chrono_atomic);
+  chrono_stop(&chrono);
   err = cudaGetLastError();
   if ( CHECK(err != cudaSuccess, "Failed to launch reduceMax_persist kernel (error code %s)!\n", cudaGetErrorString(err)) )
     exit(EXIT_FAILURE);
@@ -134,11 +144,34 @@ int main(int argc, char **argv) {
   } else { printf("Max value: %.6f\n", h_max); }
 
   printf("Tempo médio por ativação do kernel" );
-  chrono_report_TimeInLoop( &chrono_atomic, (char *)"reduceMax_atomic_persist", nR);
+  chrono_report_TimeInLoop( &chrono, (char *)"reduceMax_atomic_persist", nR);
 
-  double atomic_time_seconds = (double) chrono_gettotal( &chrono_atomic )/((double)1000*1000*1000);
+  double atomic_time_seconds = (double) chrono_gettotal( &chrono )/((double)1000*1000*1000);
   printf( "Total_time_in_seconds: %lf s\n", atomic_time_seconds );
   printf( "Throughput: %lf INT/s\n", (numElements)/atomic_time_seconds );
+
+  // EXECUTE THRUST =============================
+
+  printf("\n === EXECUTANDO KERNEL THRUST ===\n");
+  chrono_reset(&chrono);
+  chrono_start( &chrono );
+  for (int i = 0; i < nR; ++i)
+    h_max = *(thrust::max_element(thrust_d_input.begin(), thrust_d_input.end()));
+  cudaDeviceSynchronize();
+  chrono_stop( &chrono );
+
+  // Verify that the result is correct
+  if ( max != h_max ) {
+    fprintf(stderr, "Result verification failed!\n");
+    exit(EXIT_FAILURE);
+  } else { printf("Max value: %.6f\n", h_max); }
+
+  printf("Tempo médio por ativação do kernel" );
+  chrono_report_TimeInLoop( &chrono, (char *)"thrust max_element", nR);
+
+  double thrust_time_seconds = (double) chrono_gettotal( &chrono )/((double)1000*1000*1000);
+  printf( "Total_time_in_seconds: %lf s\n", thrust_time_seconds );
+  printf( "Throughput: %lf INT/s\n", (numElements)/thrust_time_seconds );
 
   // FINALIZA ===================================
 
