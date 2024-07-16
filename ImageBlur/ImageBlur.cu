55a93b5ac68a47f01196331755fe994084d551cb
Murikigy <mgy20@inf.ufpr.br>
2024-07-16 19:09:32 -0300

    Ajustado erros do wagner

--- ImageBlur/ImageBlur.cu
+++ ImageBlur/ImageBlur.cu
@@ -1,7 +1,11 @@
 // v0.2 modified by WZ
 
+#include <stdio.h>
+
 //#include <wb.h>
 #include "wb4.h" // use our lib instead (under construction)
+
+typedef unsigned int u_int;
 
 #define wbCheck(stmt)                                                     \
   do {                                                                    \
@@ -16,7 +20,26 @@
 #define BLUR_SIZE 5
 
 //@@ INSERT CODE HERE
-  //@@ INSERIR AQUI o codigo do seu kernel CUDA
+//@@ INSERIR AQUI o codigo do seu kernel CUDA
+
+
+__global__ void rgb2uintKernelSHM(unsigned int *argb, unsigned int *rgb, int width, int height){
+
+
+}
+
+
+__global__ void blurKernelSHM( unsigned int *argb_out, unsigned int *argb_in, int width, int height){
+
+
+}
+
+
+__global__ void uint2rgbKernelSHM(unsigned int *argb, unsigned int *rgb, int width, int height){
+
+
+}
+
 
 
 int main(int argc, char *argv[]) {
@@ -33,16 +56,16 @@ int main(int argc, char *argv[]) {
 
   args = wbArg_read(argc, argv); /* parse the input arguments */
 
-  inputImageFile = wbArg_getInputFile(args, 2);
+  inputImageFile = wbArg_getInputFile(args, 1);
   printf( "imagem de entrada: %s\n", inputImageFile );
 
-//  inputImage = wbImportImage(inputImageFile);
+  //  inputImage = wbImportImage(inputImageFile);
   inputImage = wbImport(inputImageFile);
 
   imageWidth  = wbImage_getWidth(inputImage);
   imageHeight = wbImage_getHeight(inputImage);
 
-// NOW: input and output images are RGB (3 channel)
+  // NOW: input and output images are RGB (3 channel)
   outputImage = wbImage_new(imageWidth, imageHeight, 3);
 
   hostInputImageData  = wbImage_getData(inputImage);
@@ -51,16 +74,12 @@ int main(int argc, char *argv[]) {
   wbTime_start(GPU, "Doing GPU Computation (memory + compute)");
 
   wbTime_start(GPU, "Doing GPU memory allocation");
-  cudaMalloc((void **)&deviceInputImageData,
-             imageWidth * imageHeight * sizeof(unsigned char) * 3);
-  cudaMalloc((void **)&deviceOutputImageData,
-             imageWidth * imageHeight * sizeof(unsigned char) * 3);
+  cudaMalloc((void **)&deviceInputImageData, imageWidth * imageHeight * sizeof(unsigned char) * 3);
+  cudaMalloc((void **)&deviceOutputImageData, imageWidth * imageHeight * sizeof(unsigned char) * 3);
   wbTime_stop(GPU, "Doing GPU memory allocation");
 
   wbTime_start(Copy, "Copying data to the GPU");
- cudaMemcpy(deviceInputImageData, hostInputImageData,
-            imageWidth * imageHeight * sizeof(unsigned char) * 3),
-            cudaMemcpyHostToDevice);
+  cudaMemcpy(deviceInputImageData, hostInputImageData, imageWidth * imageHeight * sizeof(unsigned char) * 3, cudaMemcpyHostToDevice);
 
   wbTime_stop(Copy, "Copying data to the GPU");
 
@@ -70,13 +89,23 @@ int main(int argc, char *argv[]) {
   //@@ INSERT CODE HERE
   //@@ INSERIR AQUI SEU codigo para ativar SEU kernel CUDA
 
+
+
+  rgb2uintKernelSHM<<<GRID1, NT1>>>(unsigned int *argb, unsigned int *rgb, int width, int height);
+
+
+  blurKernelSHM<<<yourGrid, yourBlocks>>>( unsigned int *argb_out, unsigned int *argb_in, int width, int height);
+
+
+  uint2rgbKernelSHM<<<GRID1, NT1>>>(unsigned int *argb, unsigned int *rgb, int width, int height);
+
+
+
   wbTime_stop(Compute, "Doing the computation on the GPU");
 
   ///////////////////////////////////////////////////////
   wbTime_start(Copy, "Copying data from the GPU");
-  cudaMemcpy(hostOutputImageData, deviceOutputImageData,
-             imageWidth * imageHeight * sizeof(unsigned char) * 3),
-             cudaMemcpyDeviceToHost);
+  cudaMemcpy(hostOutputImageData, deviceOutputImageData, imageWidth * imageHeight * sizeof(unsigned char) * 3, cudaMemcpyDeviceToHost);
 
   wbTime_stop(Copy, "Copying data from the GPU");
 
