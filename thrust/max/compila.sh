45ceb82c1c7c06d9dadc2db196ff8d4bb1e1952f
Loi-Luiz Fernando <loi7777@hotmail.com>
2024-07-16 18:42:11 -0300

    removido arquivos antigos

--- thrust/max/compila.sh
+++
@@ -1,6 +0,0 @@
-echo "----- compilando especificamente para a GTX 1080ti  (sm_61)"
-echo "nvcc -arch sm_61 --std=c++14 -I /usr/include/c++/10 -I /usr/lib/cuda/include/ thrust-max.cu -o thrust-max"
-nvcc -arch sm_61 --std=c++14 -I /usr/include/c++/10 -I /usr/lib/cuda/include/ thrust-max.cu -o thrust-max
-
-#OBS para compilar para qualquer GPU basta retirar o -arch sm_61
-#    mas isso pode deixar a compilacao (ou a carga do programa) mais lenta
