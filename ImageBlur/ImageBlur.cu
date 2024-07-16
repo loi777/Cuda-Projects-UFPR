7a6a4c1938abca28e6042007c11f9e247782d645
Loi-Luiz Fernando <loi7777@hotmail.com>
2024-07-16 18:47:31 -0300

    Adicionado T2

---
+++ ImageBlur/ImageBlur.cu
@@ -0,0 +1,97 @@
+// v0.2 modified by WZ
+
+//#include <wb.h>
+#include "wb4.h" // use our lib instead (under construction)
+
+#define wbCheck(stmt)                                                     \
+  do {                                                                    \
+    cudaError_t err = stmt;                                               \
+    if (err != cudaSuccess) {                                             \
+      wbLog(ERROR, "Failed to run stmt ", #stmt);                         \
+      wbLog(ERROR, "Got CUDA error ...  ", cudaGetErrorString(err));      \
+      return -1;                                                          \
+    }                                                                     \
+  } while (0)
+
+#define BLUR_SIZE 5
+
+//@@ INSERT CODE HERE
+  //@@ INSERIR AQUI o codigo do seu kernel CUDA
+
+
+int main(int argc, char *argv[]) {
+  wbArg_t args;
+  int imageWidth;
+  int imageHeight;
+  char *inputImageFile;
+  wbImage_t inputImage;
+  wbImage_t outputImage;
+  unsigned char *hostInputImageData;
+  unsigned char *hostOutputImageData;
+  unsigned char *deviceInputImageData;
+  unsigned char *deviceOutputImageData;
+
+  args = wbArg_read(argc, argv); /* parse the input arguments */
+
+  inputImageFile = wbArg_getInputFile(args, 2);
+  printf( "imagem de entrada: %s\n", inputImageFile );
+
+//  inputImage = wbImportImage(inputImageFile);
+  inputImage = wbImport(inputImageFile);
+
+  imageWidth  = wbImage_getWidth(inputImage);
+  imageHeight = wbImage_getHeight(inputImage);
+
+// NOW: input and output images are RGB (3 channel)
+  outputImage = wbImage_new(imageWidth, imageHeight, 3);
+
+  hostInputImageData  = wbImage_getData(inputImage);
+  hostOutputImageData = wbImage_getData(outputImage);
+
+  wbTime_start(GPU, "Doing GPU Computation (memory + compute)");
+
+  wbTime_start(GPU, "Doing GPU memory allocation");
+  cudaMalloc((void **)&deviceInputImageData,
+             imageWidth * imageHeight * sizeof(unsigned char) * 3);
+  cudaMalloc((void **)&deviceOutputImageData,
+             imageWidth * imageHeight * sizeof(unsigned char) * 3);
+  wbTime_stop(GPU, "Doing GPU memory allocation");
+
+  wbTime_start(Copy, "Copying data to the GPU");
+ cudaMemcpy(deviceInputImageData, hostInputImageData,
+            imageWidth * imageHeight * sizeof(unsigned char) * 3),
+            cudaMemcpyHostToDevice);
+
+  wbTime_stop(Copy, "Copying data to the GPU");
+
+  ///////////////////////////////////////////////////////
+  wbTime_start(Compute, "Doing the computation on the GPU");
+
+  //@@ INSERT CODE HERE
+  //@@ INSERIR AQUI SEU codigo para ativar SEU kernel CUDA
+
+  wbTime_stop(Compute, "Doing the computation on the GPU");
+
+  ///////////////////////////////////////////////////////
+  wbTime_start(Copy, "Copying data from the GPU");
+  cudaMemcpy(hostOutputImageData, deviceOutputImageData,
+             imageWidth * imageHeight * sizeof(unsigned char) * 3),
+             cudaMemcpyDeviceToHost);
+
+  wbTime_stop(Copy, "Copying data from the GPU");
+
+  wbTime_stop(GPU, "Doing GPU Computation (memory + compute)");
+
+  wbSolution(args, outputImage);
+  // DEBUG: if you want to see your image, 
+  //   will generate file bellow in current directory
+  wbExport( "blurred.ppm", outputImage );
+
+  cudaFree(deviceInputImageData);
+  cudaFree(deviceOutputImageData);
+
+  wbImage_delete(outputImage);
+  wbImage_delete(inputImage);
+
+  return 0;
+}
